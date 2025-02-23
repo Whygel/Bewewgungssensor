d220b89a720e6112141cc851d2df1efd7dfbfc43
s4004853 <s4004853>
2025-02-23 23:19:35 +0100

    Cobstaclles

---
+++ Bewewgungssensor/Obstackle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bewewgungssensor
+{
+    public  class Obstackle
+    {
+        public Border Border;
+        public Obstackle( Border iBorder)
+        {
+            
+                if (iBorder != null)
+            {
+                Border = iBorder;
+                BorderX = iBorder.X;
+                BorderY = iBorder.Y;
+            }
+        }
+        public double BorderX;
+        public double BorderY;
+    }
+}
--- Bewewgungssensor/Spielfeld.xaml.cs
+++ Bewewgungssensor/Spielfeld.xaml.cs
@@ -3,6 +3,8 @@ using Microsoft.Maui.Dispatching;
 using System.Runtime.CompilerServices;
 using Microsoft.Maui.Devices.Sensors;
 using System.Text.RegularExpressions;
+using Microsoft.Maui.Layouts;
+using GameplayKit;
 
 public partial class Spielfeld : ContentPage
 {
@@ -13,15 +15,7 @@ public partial class Spielfeld : ContentPage
 	bool GameOver = false;
 
 	private const double Speed = 10;
-	private float poxX;
-	private float poxY;
-    float NeigungX;
-    float NeigungY;
-    float InitialNeigungX;
-    float InitialNeigungY;
-    string Koordinaten;
-    float NewX;
-    float NewY;
+    int Score  = 0;
     public Spielfeld(string Spielername)
 	{
 		InitializeComponent();
@@ -58,6 +52,7 @@ public partial class Spielfeld : ContentPage
 
     double SpielerX;
     double SpielerY;
+    List<Border> ListOfObstacle = new List<Border>();
 
     bool SavedInitialPosition = false;
 
@@ -66,7 +61,16 @@ public partial class Spielfeld : ContentPage
 
         double SpielerX = SpielfeldLayout.Width;
         double SpielerY = SpielfeldLayout.Height;
+    
+        int CountObstacle = 3;
         ToggleAccelerometer();
+
+        for (int i = 0; i <= CountObstacle; i++)
+        {
+            {
+                CreateObstacle();
+            }
+        }
         await Task.Run(async () =>
         {
             while (!GameOver)
@@ -81,6 +85,42 @@ public partial class Spielfeld : ContentPage
             await Navigation.PushAsync(new Menue());
         });
     }
+
+    Random RandomX = new Random();
+    Random RandomY = new Random();
+
+    int x;
+    int y;
+
+
+    private void CreateObstacle()
+    {
+        Border newObstacle = new Border ()
+        {
+            BackgroundColor = Colors.Red,
+            WidthRequest = 50,
+            HeightRequest = 50
+        };
+        ListOfObstacle.Add(newObstacle);
+
+        foreach (Border obstacle in ListOfObstacle)
+        {
+            if (obstacle.Parent != null)
+            {
+                ((AbsoluteLayout)obstacle.Parent).Children.Remove(obstacle);
+            }
+            x = RandomX.Next(0, (int)SpielfeldLayout.Height);
+            y = RandomY.Next(0, (int)SpielfeldLayout.Width);
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                AbsoluteLayout.SetLayoutBounds(obstacle, new Rect(x, y, obstacle.Width, obstacle.Height));
+                AbsoluteLayout.SetLayoutFlags(obstacle, AbsoluteLayoutFlags.None);
+                SpielfeldLayout.Add(obstacle);
+            });           
+        }
+    }
+
     double newPosX;
     double newPosY;
     private void CalculateMovement()
@@ -106,6 +146,29 @@ public partial class Spielfeld : ContentPage
         {
             SpielfeldLayout.SetLayoutBounds(Spieler, new Rect(newPosX ,newPosY, Spieler.Width, Spieler.Height));
         });
+
+
+       foreach(var obstacle in ListOfObstacle)
+        {
+            if (CheckCollision(obstacle))
+            {
+                Score += 100;
+                TbScore.Text = $"Score: {score}";
+                return;
+            }
+        }
+
+        // Kolliosinhandlung 
+
+
+    }
+
+    private bool CheckCollision(Border obstacle)
+    {
+        var playerBounds = AbsoluteLayout.GetLayoutBounds(Spieler);
+        var obstacleBounds = AbsoluteLayout.GetLayoutBounds(obstacle);
+
+        return (Math.Abs(playerBounds.X - obstacleBounds.X) < 0.1) && (Math.Abs(playerBounds.Y - obstacleBounds.Y) < 0.1);
     }
 
     public void ToggleAccelerometer()
@@ -182,5 +245,13 @@ public partial class Spielfeld : ContentPage
     {
         Accelerometer.Default.Stop();
         Accelerometer.Default.ReadingChanged -= Default_ReadingChanged;
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            await Navigation.PushAsync(new Menue());
+        });
     }
 }
